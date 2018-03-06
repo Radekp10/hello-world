c0747da8d1354a93bd89c4e851b036e4c5fb7fe2
Radekp10 <37044117+Radekp10@users.noreply.github.com>
2018-03-06 04:40:25 +0100

    Add files via upload

---
+++ header.h
@@ -0,0 +1,7 @@
+//plik header.h
+
+class Napis
+{
+public:
+	void pisz() {using namespace std; cout << "Hello world" << endl; }
+};
---
+++ main.cpp
@@ -0,0 +1,11 @@
+//plik main.cpp
+
+#include <iostream>
+#include "header.h"
+
+int main()
+{
+	Napis hello;		//stworzenie obiektu klasy Napis o nazwie hello
+	hello.pisz();
+	return 0;
+}
